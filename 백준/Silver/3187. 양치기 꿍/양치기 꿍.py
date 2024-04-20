a0f9d242a6a197f9f4c60f65c607a0f182c45c9d
sojeong32 <141535118+sojeong32@users.noreply.github.com>
2024-04-20 15:37:26 +0900

    [Silver I] Title: 양치기 꿍, Time: 188 ms, Memory: 31512 KB -BaekjoonHub

---
+++ 백준/Silver/3187. 양치기 꿍/양치기 꿍.py
@@ -0,0 +1,43 @@
+r, c = map(int, input().split())
+arr = [list(input()) for _ in range(r)]
+visited = [[0]*c for _ in range(r)]
+q = []
+di = [0, 1, 0, -1]
+dj = [1, 0, -1, 0]
+
+total_lamb = 0
+total_wolf = 0
+for i in range(1, r-1):
+    for j in range(1, c-1):
+        if (arr[i][j] == 'v' or arr[i][j] == 'k') and visited[i][j] == 0:
+            lamb_cnt = 0
+            wolf_cnt = 0
+            if arr[i][j] == 'v':
+                wolf_cnt = 1
+            else:
+                lamb_cnt = 1
+            visited[i][j] = 1
+            q.append([i, j])
+
+            while q:
+                x, y = q.pop(0)
+                for k in range(4):
+                    ni = x + di[k]
+                    nj = y + dj[k]
+                    if 1 <= ni < r-1 and 1 <= nj < c-1 and (arr[ni][nj] == 'v' or arr[ni][nj] == '.') and visited[ni][nj] == 0:
+                        if arr[ni][nj] == 'v':
+                            wolf_cnt += 1
+                        visited[ni][nj] = 1
+                        q.append([ni, nj])
+                    if 1 <= ni < r-1 and 1 <= nj < c-1 and (arr[ni][nj] == 'k' or arr[ni][nj] == '.') and visited[ni][nj] == 0:
+                        if arr[ni][nj] == 'k':
+                            lamb_cnt += 1
+                        visited[ni][nj] = 1
+                        q.append([ni, nj])
+
+            if lamb_cnt > wolf_cnt:
+                total_lamb += lamb_cnt
+            else:
+                total_wolf += wolf_cnt
+
+print(total_lamb, total_wolf)
