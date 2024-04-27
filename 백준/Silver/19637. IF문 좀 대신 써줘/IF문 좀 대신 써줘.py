9d3978a24c40d0f5cdb0d6c9720dd29362b65269
sojeong32 <141535118+sojeong32@users.noreply.github.com>
2024-04-27 20:50:47 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 704 ms, Memory: 49308 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.py
@@ -10,15 +10,13 @@ for i in range(N):
 
 for k in range(M):
     power = int(sys.stdin.readline())
-    s, e = 0, N-1
+    s, e = 0, N
     while s <= e:
         middle = (s+e)//2
         if power > power_list[middle][1]:
             s = middle + 1
         elif power <= power_list[middle][1]:
             e = middle - 1
-        else:
-            break
 
     ans = s
     print(power_list[ans][0])
