9c24f6db043e628c7c3566437fe814370d918aac
sojeong32 <141535118+sojeong32@users.noreply.github.com>
2024-05-08 01:49:33 +0900

    [Gold V] Title: 적록색약, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,3 +1,18 @@
+def check(color):
+    global cnt
+    if not visited[i][j] and arr[i][j] == color:
+        q.append([i, j])
+        visited[i][j] = 1
+        while q:
+            x, y = q.pop(0)
+            for k in range(4):
+                ni = x + di[k]
+                nj = y + dj[k]
+                if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and arr[ni][nj] == color:
+                    visited[ni][nj] = 1
+                    q.append([ni, nj])
+        cnt += 1
+
 N = int(input())
 arr = [list(input()) for _ in range(N)]
 visited = [[0]*N for _ in range(N)]
@@ -10,44 +25,9 @@ ans2 = 0
 for i in range(N):
     for j in range(N):
         cnt = 0
-        if not visited[i][j] and arr[i][j] == 'R':
-            q.append([i, j])
-            visited[i][j] = 1
-            while q:
-                x, y = q.pop(0)
-                for k in range(4):
-                    ni = x + di[k]
-                    nj = y + dj[k]
-                    if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and arr[ni][nj] == 'R':
-                        visited[ni][nj] = 1
-                        q.append([ni, nj])
-            cnt += 1
-
-        if not visited[i][j] and arr[i][j] == 'B':
-            q.append([i, j])
-            visited[i][j] = 1
-            while q:
-                x, y = q.pop(0)
-                for k in range(4):
-                    ni = x + di[k]
-                    nj = y + dj[k]
-                    if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and arr[ni][nj] == 'B':
-                        visited[ni][nj] = 1
-                        q.append([ni, nj])
-            cnt += 1
-
-        if not visited[i][j] and arr[i][j] == 'G':
-            q.append([i, j])
-            visited[i][j] = 1
-            while q:
-                x, y = q.pop(0)
-                for k in range(4):
-                    ni = x + di[k]
-                    nj = y + dj[k]
-                    if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and arr[ni][nj] == 'G':
-                        visited[ni][nj] = 1
-                        q.append([ni, nj])
-            cnt += 1
+        check('R')
+        check('B')
+        check('G')
         ans1 += cnt
 
 visited = [[0]*N for _ in range(N)]
@@ -66,19 +46,7 @@ for i in range(N):
                         visited[ni][nj] = 1
                         q.append([ni, nj])
             cnt += 1
-
-        if not visited[i][j] and arr[i][j] == 'B':
-            q.append([i, j])
-            visited[i][j] = 1
-            while q:
-                x, y = q.pop(0)
-                for k in range(4):
-                    ni = x + di[k]
-                    nj = y + dj[k]
-                    if 0 <= ni < N and 0 <= nj < N and visited[ni][nj] == 0 and arr[ni][nj] == 'B':
-                        visited[ni][nj] = 1
-                        q.append([ni, nj])
-            cnt += 1
+        check('B')
         ans2 += cnt
 
 print(ans1, ans2)
